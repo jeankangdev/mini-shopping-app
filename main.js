06f0abc4ded371942acd69b8fefbee051a4d2eb6
Jean Kang <jeankang.dev@gmail.com>
2021-03-08 20:45:04 -0500

    display json data

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 function loadItems() {
   return fetch('data/data.json')  // fetch returns response if it's fetched successfully
     .then(response => response.json()) // response body -> json obj
-    .then(json => console.log(json));
+    .then(json => json.items);
 }
 
 // main
 loadItems() // returns Promise if it was successful
   .then(items => {
+    console.log(items);
     // displayItems(items);
     // setEventListeners(items);
   })
